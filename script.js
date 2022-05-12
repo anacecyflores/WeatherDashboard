b4cf076b1f5b0561162b747f9963e08d565d4f43
Cecy Flores <anacecyflores1@gmail.com>
2022-05-11 20:43:52 -0500

    Added Uv Index Api and Fucntions for data

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 var userInput = document.getElementById("inputValue");
 var search = document.getElementById("searchBtn");
 search.addEventListener("click", function () {
-  console.log(userInput.value);
+  // console.log(userInput.value);
   currentWeatherAPI(userInput.value);
 });
+function onecallAPI(lattitude, longitude) {
+  console.log("inside Onecall", lattitude, longitude);
+  var uvApi =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    lattitude +
+    "&lon=" +
+    longitude +
+    "&appid=5800a31e16468ce7978a067a48244cb0&units=imperial&exclude=minutely,hourly,alerts";
+
+  fetch(uvApi)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (uviData) {
+      console.log(uviData);
+    });
+}
 function currentWeatherAPI(location) {
   const weatherApi =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -11,21 +28,22 @@ function currentWeatherAPI(location) {
     "&appid=5800a31e16468ce7978a067a48244cb0&units=imperial";
   fetch(weatherApi)
     .then(function (response) {
-      console.log("response", response);
+      // console.log("response", response);
       return response.json();
     })
     .then(function (data) {
       console.log("data", data);
+      var cityNameEl = document.querySelector(".cityName");
+      cityNameEl.textContent = data.name;
+      var tempEl = document.querySelector(".temperature");
+      tempEl.textContent = data.main.temp + " ℉";
+      var humidityEl = document.querySelector(".humidity");
+      humidityEl.textContent = data.main.humidity;
+      console.log("Coord inside then", data.coord.lon, data.coord.lat);
+      onecallAPI(data.coord.lat, data.coord.lon);
+      // Var uvIndex
+      // console.log("name", data.name);
+      // console.log("temp", data.main.temp);
+      // console.log("humidity", data.main.humidity);
     });
 }
-
-// const weatherApi =
-//   "https://api.openweathermap.org/data/2.5/weather?q=london&appid=5800a31e16468ce7978a067a48244cb0&units=imperial";
-// fetch(weatherApi)
-//   .then(function (response) {
-//     console.log("response", response);
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log("data", data);
-//   });
